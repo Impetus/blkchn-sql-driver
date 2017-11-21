e1f457155644e4301cd843bcc85ad972b3427182
Hosur Narahari <hosur.narhari@impetus.co.in>
2017-11-21 11:28:44 +0530

    committing supported for star item in select clause

---
+++ blkchn-sql-interfaces/src/main/java/com/impetus/blkch/sql/query/StarNode.java
@@ -0,0 +1,28 @@
+package com.impetus.blkch.sql.query;
+
+import com.impetus.blkch.sql.parser.TreeNode;
+
+public class StarNode extends TreeNode {
+	
+	public static final String DESCRIPTION = "STAR";
+	
+	private String tableIdentifier;
+
+	public StarNode() {
+		this(null);
+	}
+	
+	public StarNode(String tableIdentifier) {
+		super(DESCRIPTION + ":" + ((tableIdentifier != null) ? tableIdentifier + ".*" : "*"));
+		this.tableIdentifier = tableIdentifier;
+	}
+
+	public String getTableIdentifier() {
+		return tableIdentifier;
+	}
+
+	public void setTableIdentifier(String tableIdentifier) {
+		this.tableIdentifier = tableIdentifier;
+	}
+
+}
