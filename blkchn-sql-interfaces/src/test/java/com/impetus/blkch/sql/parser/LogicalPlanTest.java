824e66ee725c3a798f153a96b832328e54ceeee1
jeetendra.jaiswal <jeetendra.jaiswal@impetus.co.in>
2018-03-13 12:00:22 +0530

    Unit test for AbstractAssetManager

--- blkchn-sql-interfaces/src/test/java/com/impetus/blkch/sql/parser/LogicalPlanTest.java
+++ blkchn-sql-interfaces/src/test/java/com/impetus/blkch/sql/parser/LogicalPlanTest.java
@@ -324,19 +324,6 @@ public class LogicalPlanTest extends TestCase {
         LogicalPlan plan = getLogicalPlan(sql);
         plan.getQuery().traverse();
     }
-    
-//    @Test
-//    public void testCreateAsset() {
-//        String sql = "CREATE ASSET user_asset FOR chaincode assetchain and FUNCTION qGetUsers ("
-//                + "ID int,"
-//                + "AssetType string,"
-//                + "Email string)"
-//                + " WITH STORAGE TYPE CSV "
-//                + "FIELDS DELIMITED BY ',' "
-//                + "RECORDS DELIMITED BY \"\\n\"";
-//        LogicalPlan plan = getLogicalPlan(sql);
-//        plan.getCreateAsset().traverse();
-//    }
 
     @Test
     public void testCreateAssetWithJSONStorage() {
