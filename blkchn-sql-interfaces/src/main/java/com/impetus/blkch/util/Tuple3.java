090014ef2ea270644704098ad67a0078afc0d105
Hosur Narahari <hosur.narhari@impetus.co.in>
2018-04-01 20:47:19 +0530

    committing BlkchnArray and Tuple3 classes

--- blkchn-sql-interfaces/src/main/java/com/impetus/blkch/util/Tuple3.java
+++ blkchn-sql-interfaces/src/main/java/com/impetus/blkch/util/Tuple3.java
@@ -1,31 +1,38 @@
 package com.impetus.blkch.util;
 
-public class Tuple2<T1, T2> {
-
+public class Tuple3<T1, T2, T3> {
+    
     private T1 first;
     
     private T2 second;
     
-    public Tuple2(T1 first, T2 second) {
+    private T3 third;
+    
+    public Tuple3(T1 first, T2 second, T3 third) {
         this.first = first;
         this.second = second;
+        this.third = third;
     }
-    
+
     public T1 getFirst() {
         return first;
     }
-    
+
     public T2 getSecond() {
         return second;
+    }
+
+    public T3 getThird() {
+        return third;
     }
     
     @Override
     public boolean equals(Object obj) {
-        if(!(obj instanceof Tuple2<?,?>)) {
+        if(!(obj instanceof Tuple3<?,?,?>)) {
             return false;
         }
-        Tuple2<?, ?> other = (Tuple2<?,?>)obj;
-        return this.first.equals(other.first) && this.second.equals(other.second);
+        Tuple3<?, ?, ?> other = (Tuple3<?,?,?>)obj;
+        return this.first.equals(other.first) && this.second.equals(other.second) && this.third.equals(other.third);
     }
     
     @Override
@@ -33,6 +40,8 @@ public class Tuple2<T1, T2> {
         int hashCode = 31;
         hashCode = 53 * (hashCode + first.hashCode());
         hashCode = 53 * (hashCode + second.hashCode());
+        hashCode = 53 * (hashCode + third.hashCode());
         return hashCode;
     }
+
 }
